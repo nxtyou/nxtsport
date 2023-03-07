346aeed09ef6b5700ccb7dcdd4484a319dd2c258
yamanidev <elamine.yamani@gmail.com>
2023-03-07 21:40:22 +0100

    Added Eurostar Black Extended font to configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,17 @@ module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      fontFamily: { inter: ['Inter', 'sans-serif'], poppins: ['Poppins', 'sans-serif'] },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+        'eurostar-black-extended': ['Eurostar Black Extended', 'sans-serif']
+      },
       colors: {
         black: '#040617',
-        white: '#F5F5F5',
         'prussian-blue': '#002347',
         'international-orange': '#FF5003',
         'regal-blue': '#003F7D',
-        iron: '#DCDCDE'
+        iron: '#D8D8D8'
       }
     }
   },
