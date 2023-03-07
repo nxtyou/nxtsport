eb385054ee0e1d9f301f82e1e1ff20b640ea172c
yamanidev <elamine.yamani@gmail.com>
2023-03-07 22:33:31 +0100

    Changed background image technique + improved blur and dim

--- components/templates/ComingSoon/Hero.jsx
+++ components/templates/ComingSoon/Hero.jsx
@@ -1,4 +1,34 @@
+import ButtonLink from 'components/atoms/ButtonLink';
+import ArrowIcon from 'components/atoms/icons/ArrowIcon';
+
 function Hero() {
-  return <section className="hero h-screen"></section>;
+  return (
+    <section className="relative flex h-screen items-center justify-center overflow-hidden">
+      <div className="hero-bg">
+        <div className="blur-bg"></div>
+      </div>
+      <div className="text-center">
+        <img
+          src="/images/exploding-head-emoji.svg"
+          width={132}
+          alt="exploding head emoji"
+          className="mx-auto w-20 sm:w-32"
+        />
+        <h1 className="mt-8 max-w-4xl font-eurostar-black-extended text-4xl tracking-[-0.08em] sm:text-5xl sm:leading-[90.5%]">
+          WE CREATE AWESOME STUFF FOR YOU.
+        </h1>
+        <p className="mt-4 text-base text-iron sm:text-lg">
+          We are going to launch our website soon.
+        </p>
+        <ButtonLink
+          href="/"
+          className="group mx-auto mt-10 flex"
+          rightIcon={<ArrowIcon animated />}
+          big>
+          Hier zum Kontakt
+        </ButtonLink>
+      </div>
+    </section>
+  );
 }
 export default Hero;
