01920d50b768cbeb6662766cd5096a83efc1ef0e
yamanidev <elamine.yamani@gmail.com>
2023-03-10 23:15:43 +0100

    Used BackgroundBlur component

--- components/templates/ComingSoon/Hero.jsx
+++ components/templates/ComingSoon/Hero.jsx
@@ -1,3 +1,4 @@
+import BackgroundBlur from 'components/atoms/BackgroundBlur';
 import ButtonLink from 'components/atoms/ButtonLink';
 import ArrowIcon from 'components/atoms/icons/ArrowIcon';
 import SocialLinks from 'components/shared/SocialLinks';
@@ -5,9 +6,7 @@ import SocialLinks from 'components/shared/SocialLinks';
 function Hero() {
   return (
     <section className="relative flex h-screen items-center justify-center overflow-hidden">
-      <div className="hero-bg">
-        <div className="blur-bg"></div>
-      </div>
+      <BackgroundBlur />
       <div className="px-2 text-center sm:px-0">
         <img
           src="/images/exploding-head-emoji.svg"
@@ -15,12 +14,14 @@ function Hero() {
           alt="exploding head emoji"
           className="mx-auto w-20 sm:w-32"
         />
-        <h1 className="mt-8 max-w-4xl cursor-default font-eurostar-black-extended text-4xl tracking-[-0.08em] sm:text-5xl sm:leading-[90.5%]">
-          WE CREATE AWESOME STUFF FOR YOU.
-        </h1>
-        <p className="mt-4 cursor-default text-base text-iron sm:text-lg">
-          We are going to launch our website soon.
-        </p>
+        <div className="max-w-4xl">
+          <h1 className="mt-8 cursor-default font-eurostar-black-extended text-4xl tracking-[-0.08em] sm:text-5xl sm:leading-[90.5%]">
+            WE CREATE AWESOME STUFF FOR YOU.
+          </h1>
+          <p className="mt-4 cursor-default text-base text-iron sm:text-lg">
+            We are going to launch our website soon.
+          </p>
+        </div>
         <ButtonLink
           href="/"
           className="group mx-auto mt-10 flex"
