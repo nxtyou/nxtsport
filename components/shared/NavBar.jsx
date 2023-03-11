f626bfd0555742e3a3d88319fa262f2098588b19
yamanidev <elamine.yamani@gmail.com>
2023-03-11 19:17:20 +0100

    Using rem instead of px

--- components/shared/NavBar.jsx
+++ components/shared/NavBar.jsx
@@ -7,7 +7,13 @@ function NavBar({ ctaText, ctaTarget }) {
     <header className="absolute top-8 right-0 left-0 z-10">
       <div className="mx-auto flex max-w-[1340px] items-center justify-between px-2 sm:px-4 xl:px-0">
         <Link href="/">
-          <img src="/images/logo.png" alt="nxt sport logo" width={73} height={59} />
+          <img
+            src="/images/logo.png"
+            alt="nxt sport logo"
+            className="h-[3.75rem] w-[4.625rem]"
+            width={73}
+            height={59}
+          />
         </Link>
         <ButtonLink
           href={ctaTarget}
