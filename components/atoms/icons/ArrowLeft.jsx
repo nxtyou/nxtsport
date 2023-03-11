5e37de65cf4d340924e0a9fe2d1494064f96a3d8
yamanidev <elamine.yamani@gmail.com>
2023-03-11 22:27:53 +0100

    Added ArrowLeft icon

---
+++ components/atoms/icons/ArrowLeft.jsx
@@ -0,0 +1,16 @@
+function ArrowLeft({ className, animated }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={3}
+      stroke="currentColor"
+      className={`transition-all ${animated ? 'group-hover:-translate-x-1' : ''} ${
+        className || ''
+      }`}>
+      <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
+    </svg>
+  );
+}
+export default ArrowLeft;
