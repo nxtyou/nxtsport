87b03820f2f03d4be92174e3482796127a6a6e56
yamanidev <elamine.yamani@gmail.com>
2023-03-11 13:56:54 +0100

    Added hover effect to Footer links

--- components/shared/Footer.jsx
+++ components/shared/Footer.jsx
@@ -3,10 +3,14 @@ import Link from 'next/link';
 function Footer() {
   return (
     <footer className="absolute bottom-0 left-0 right-0 text-sm">
-      <div className="flex justify-end gap-1 pr-3 pb-3 opacity-50">
-        <Link href="/">IMPRESSUM</Link>
+      <div className="flex justify-end gap-1 pr-3 pb-3">
+        <Link href="/" className="opacity-50 transition-all hover:opacity-80">
+          IMPRESSUM
+        </Link>
         <span className="cursor-default">|</span>
-        <Link href="/">DATENSCHUTZ</Link>
+        <Link href="/" className="opacity-50 transition-all hover:opacity-80">
+          DATENSCHUTZ
+        </Link>
       </div>
     </footer>
   );
