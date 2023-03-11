66c62203f5f2649fd37d45ff3f4c60e28d3b7c7c
yamanidev <elamine.yamani@gmail.com>
2023-03-11 14:11:42 +0100

    Decreased font size on mobile

--- components/shared/Footer.jsx
+++ components/shared/Footer.jsx
@@ -2,8 +2,8 @@ import Link from 'next/link';
 
 function Footer() {
   return (
-    <footer className="absolute bottom-0 left-0 right-0 text-sm">
-      <div className="flex justify-end gap-1 pr-3 pb-3">
+    <footer className="absolute bottom-0 left-0 right-0 text-xs md:text-sm">
+      <div className="flex justify-center gap-1 pr-3 pb-3 md:justify-end">
         <Link href="/" className="opacity-50 transition-all hover:opacity-80">
           IMPRESSUM
         </Link>
