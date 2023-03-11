313781de754e9d72884c3d8f0d028ecaa01eca9f
yamanidev <elamine.yamani@gmail.com>
2023-03-11 12:00:27 +0100

    Redirecting to contact + changed structure

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,4 @@
+import BackgroundBlur from 'components/atoms/BackgroundBlur';
 import Footer from 'components/shared/Footer';
 import NavBar from 'components/shared/NavBar';
 import Hero from 'components/templates/ComingSoon/Hero';
@@ -5,8 +6,9 @@ import Hero from 'components/templates/ComingSoon/Hero';
 function Home() {
   return (
     <>
-      <NavBar ctaText="Anfrage senden" ctaTarget="/" />
-      <main className="">
+      <NavBar ctaText="Anfrage senden" ctaTarget="/contact" />
+      <main>
+        <BackgroundBlur />
         <Hero />
       </main>
       <Footer />
